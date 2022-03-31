2f21e63be48b8221b6e3959634db8a1ea291cced
Kamil Wojkowski <kamil.wojkowski@ppuc.pl>
2022-03-31 16:48:13 +0200

    configure testing library

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.js'],
   globalSetup: 'jest-preset-angular/global-setup',
 };
